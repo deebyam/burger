6051d48759fe4ede7e547954bf4b868c24026840
deebyam <deebyam@hotmail.com>
2019-02-18 19:26:30 -0800

    Create burger check-in

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import classes from './BurgerIngredient.css';
+
+class BurgerIngredient extends Component {
+    render() {
+    let ingredient = null;
+
+    switch (this.props.type){
+        case ('bread-bottom'):
+            ingredient = <div className="BreadBottom"></div>;
+            break;
+        case ('bread-top'):
+            ingredient = (
+                <div className="BreadTop">
+                    <div className="Seeds1"></div>
+                    <div className="Seeds2"></div>
+                </div>
+            );
+            break;    
+        case ('Meat'):
+              ingredient = <div className="Meat"></div>;
+              break;  
+        case ('Salad'):
+              ingredient = <div className="Salad"></div>;
+              break; 
+        case ('Cheese'):
+              ingredient = <div className="Cheese"></div>;
+              break;     
+        case ('Bacon'):
+              ingredient = <div className="Bacon"></div>;
+              break;   
+        default: 
+            ingredient = null;                   
+    }
+        return ingredient;
+
+    }
+
+
+};
+
+BurgerIngredient.propTypes = {
+    type: PropTypes.string.isRequired
+}
+export default BurgerIngredient;
